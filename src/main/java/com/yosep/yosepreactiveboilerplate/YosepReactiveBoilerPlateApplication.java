c8ef9ba046048700f73d1fe6b1194869a902d1d5
yosepjeon <enekelx1@naver.com>
2022-11-10 15:10:15 +0900

    Initial commit

---
+++ src/main/java/com/yosep/yosepreactiveboilerplate/YosepReactiveBoilerPlateApplication.java
@@ -0,0 +1,13 @@
+package com.yosep.yosepreactiveboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YosepReactiveBoilerPlateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YosepReactiveBoilerPlateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yosep/yosepreactiveboilerplate/YosepReactiveBoilerPlateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yosep.yosepreactiveboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YosepReactiveBoilerPlateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
